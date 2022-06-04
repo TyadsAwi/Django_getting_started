ed7a54cb688847d18241392181e226d3b3e65763
TyadsAwi <tyads25@gmail.com>
2022-06-04 03:51:20 +0100

    First Commit

---
+++ TyadsAwi/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Tyads_Awi project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tyads_Awi.settings')
+
+application = get_asgi_application()
---
+++ TyadsAwi/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Tyads_Awi project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tyads_Awi.settings')
+
+application = get_wsgi_application()
